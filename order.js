dc6f1b54658fd3c076c08e13c391755760d1ecc5
Brijisha <brijishadoshi12@gmail.com>
2022-12-07 15:29:46 +0530

    Admin Dashboard using HTML CSS and JSis completed.

---
+++ order.js
@@ -0,0 +1,36 @@
+const Orders = [
+  {
+    productName: "Foodable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Foodable Large Drone",
+    productNumber: "95631",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+
+  {
+    productName: "Ruko F1 Pro Drone",
+    productNumber: "45501",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+
+  {
+    productName: "Drone with Camera Drone",
+    productNumber: "96699",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+
+  {
+    productName: "GPS 4k Drone",
+    productNumber: "60699",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
